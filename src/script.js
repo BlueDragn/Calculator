0e5391817bc05f8e0838835782866d7143aae553
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-20 23:34:19 +0530

    tring to  populate input field with keys

--- src/script.js
+++ src/script.js
@@ -1,17 +1,31 @@
 var inputField = document.getElementById("result-field");
 var displayNum = document.getElementById("result");
 
-var num = document.getElementById("7").value;
-console.log(num);
-// num.addEventListener("click", keyLog);
+// var num = document.getElementById("7").value;
+// console.log(num);
+// // num.addEventListener("click", keyLog);
 
- function keyLog(){
-displayNum.value = num;
- }
+//  function keyLog(){
+// displayNum.value = num;
+//  }
+var keyLog = document.querySelectorAll(".num");
+
+
+// keyLog.addEventListener("click", function(e){
+//     var  m = e.target.value;
+//     displayNum.value = m;
+// });
+
+keyLog.forEach(function (num) {
+    num.addEventListener("click", function (e) {
+      var m = e.target.value;
+      displayNum.value = m;
+    });
+  });
 
 
 
 
 function myFunction() {
     inputField.reset();
-  }
+  };
