54523729a0a225b50b69ddbf4da96cdf9c56726a
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-20 22:25:42 +0530

    added reset function

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// var num = document.querySelectorAll("num");
+// num.addEventListener("click", readMore);
+
+// console.log(num);
+
+
+function myFunction() {
+    document.getElementById("result-field").reset();
+  }
