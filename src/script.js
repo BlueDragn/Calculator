6a4bc2527fa31ecc2d691532c3c5e4d85e2cb89b
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-05-21 22:16:20 +0530

    added operatotions functions

--- src/script.js
+++ src/script.js
@@ -1,20 +1,88 @@
-
-
 var inputField = document.getElementById("result-field");
 var displayNum = document.getElementById("result");
-
 var numericButtons = document.querySelectorAll(".num");
+var operationButtons = document.querySelectorAll(".op");
 
+
+var num1 = 0;// Variable to store the first number input
+
+//event listener for Numeric button
 numericButtons.forEach(function (button) {
   button.addEventListener("click", function (e) {
-    var num = e.target.value;
-    displayNum.value += num;
+    var keyValue = e.target.value;
+    populateResultField(keyValue);
+
   });
 });
 
+//event listener for Operation button
+operationButtons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    keyValue = e.target.value;
+    var op = keyValue;
+    num1 = Number(displayNum.value); // Store the current display value as num1
+    // displayNum.value = ""; // Clear the display for the next input
+
+  });
+});
+
+//function to populate the input field
+function populateResultField(num) {
+  displayNum.value += num;
+}
 
 
-
+//Function for reset button
 function myFunction() {
-    inputField.reset();
-  };
+  inputField.reset();
+};
+
+
+/**
+ *Operations functions below
+ */
+
+//Addition
+function addition(n1, n2) {
+  return n1 + n2;
+
+}
+
+//subtraction
+function subtraction(n1, n2) {
+  return n1 - n2;
+}
+
+//Multiplication
+function multiplication(n1, n2) {
+  return n1 * n2;
+}
+
+//Division
+function division(n1, n2) {
+  return n1 / n2;
+}
+
+
+//Function to do arithmetic  operation
+function operate(num1, op, num2) {
+  var result;
+  num2 = Number(displayNum.value); // Get the second number from the display
+  if (op === "+") {
+
+    return result = addition(num1, num2);
+
+  } else if (op === "-") {
+    return result = subtraction(num1, num2);
+
+  } else if (op === "*") {
+    return result = multiplication(num1, num2);
+
+  } else if (op === "/") {
+    return result = division(num1, num2);
+
+  } else {
+    result = 0;
+  }
+  displayNum.value = result; // Display the result
+}
